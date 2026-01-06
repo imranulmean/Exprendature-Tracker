ab5334414086290206751d61e3cba976a1da5b3d
Imranul Hasan <imranulhasan73@gmail.com>
2026-01-06 12:33:20 +0600

    file upload only login

--- client/src/pages/UploadFile.jsx
+++ client/src/pages/UploadFile.jsx
@@ -125,85 +125,87 @@ export default function UploadFile(){
 
     return(
         <div className="w-full bg-gray-200">
-          <div className="p-4">
-            File uploader
-            <input type="file" accept="image/*,video/*" multiple onChange={(e)=>setFiles(e.target.files)}  ref={fileInputRef} disabled={loading}/>
-            {loading && 
-                <div>
-                    <p>Uploading...{progress}%</p>
-                    <progress value={progress} max="100" />
-                </div>
-                
-            }
-            {
-              progress==100 && <p>Processing...</p>
-            } 
-            <div className="flex gap-2 p-2">
-              <Button onClick={()=>handleUpload('uploadDrive')} disabled={loading}>Upload Drive</Button>
-              <Button color='light' onClick={()=>handleUpload('uploadLocal')} disabled={loading}>Upload Local</Button>              
-            </div>        
-
-            {
-                wakeLockActivate && <p>Wake lock Activated</p>
-            }
-          </div>
-
-
-        <div className="p-4">
-          <h2>My Drive Files ({driveFiles?.totalFiles})</h2>
-          <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
-            <ModalBody>
-              <iframe
-                src={fileLink}
-                width="100%"
-                height="500px"
-                className="rounded-lg"
-                allowFullScreen>
-              </iframe>             
-            </ModalBody>
-            <ModalFooter>
-              <Button onClick={() => setOpenModal(false)}>Close</Button>
-            </ModalFooter>            
-          </Modal>            
-          <div className="flex flex-col md:flex-row justify-center gap-2 flex-wrap">
-            {driveFiles?.fileDetails.map((file) => (
-              <div key={file.id} >              
-                <Card className=" flex w-full overflow-hidden justify-center items-center">
-                  {
-                    file.thumbnailLink ?
-                    <img src={file.thumbnailLink.replace('=s220','=s400')} 
-                          referrerPolicy="no-referrer"
-                          crossOrigin="anonymous" 
-                          // onClick={() => openFileInModal(file.webViewLink.replace('/view?usp=drivesdk', '/preview'))} 
-                          // onClick={() => openFileInModal(file.thumbnailLink.replace('=s220','=s0'))}
-                          />
-                    :
-                    <img src='https://www.biblecenterchurch.com/wp-content/uploads/2018/10/video-placeholder-300x169.png' 
-                         onClick={() => openFileInModal(file.webViewLink.replace('/view?usp=drivesdk', '/preview'))}  
-                    />
-                  }
+          {
+            currentUser &&
+            <div className="p-4">
+              File uploader
+              <input type="file" accept="image/*,video/*" multiple onChange={(e)=>setFiles(e.target.files)}  ref={fileInputRef} disabled={loading}/>
+              {loading && 
+                  <div>
+                      <p>Uploading...{progress}%</p>
+                      <progress value={progress} max="100" />
+                  </div>
                   
-                  <div className="flex gap-2">                          
-                    <Button color="light">
-                        <a href={file.webViewLink} target="_blank" rel="noreferrer">View in Drive</a>
-                    </Button>
-                    <Button  onClick={()=>  openFileInModal(file.webViewLink.replace('/view?usp=drivesdk', '/preview'))} >
-                        Full View
-                      </Button>                    
-                    {
-                      currentUser&&
-                      <Button  onClick={()=> deleteFiles(file.id)} disabled={deleting}>
-                        Delete file
-                      </Button> 
-                    }               
-                             
-                  </div> 
+              }
+              {
+                progress==100 && <p>Processing...</p>
+              } 
+              <div className="flex gap-2 p-2">
+                <Button onClick={()=>handleUpload('uploadDrive')} disabled={loading}>Upload Drive</Button>
+                <Button color='light' onClick={()=>handleUpload('uploadLocal')} disabled={loading}>Upload Local</Button>              
+              </div>        
 
-                </Card>                  
-              </div>
-            ))}
+              {
+                  wakeLockActivate && <p>Wake lock Activated</p>
+              }
+            </div>            
+          }
+
+          <div className="p-4">
+            <h2>My Drive Files ({driveFiles?.totalFiles})</h2>
+            <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+              <ModalBody>
+                <iframe
+                  src={fileLink}
+                  width="100%"
+                  height="500px"
+                  className="rounded-lg"
+                  allowFullScreen>
+                </iframe>             
+              </ModalBody>
+              <ModalFooter>
+                <Button onClick={() => setOpenModal(false)}>Close</Button>
+              </ModalFooter>            
+            </Modal>            
+            <div className="flex flex-col md:flex-row justify-center gap-2 flex-wrap">
+              {driveFiles?.fileDetails.map((file) => (
+                <div key={file.id} >              
+                  <Card className=" flex w-full overflow-hidden justify-center items-center">
+                    {
+                      file.thumbnailLink ?
+                      <img src={file.thumbnailLink.replace('=s220','=s400')} 
+                            referrerPolicy="no-referrer"
+                            crossOrigin="anonymous" 
+                            // onClick={() => openFileInModal(file.webViewLink.replace('/view?usp=drivesdk', '/preview'))} 
+                            // onClick={() => openFileInModal(file.thumbnailLink.replace('=s220','=s0'))}
+                            />
+                      :
+                      <img src='https://www.biblecenterchurch.com/wp-content/uploads/2018/10/video-placeholder-300x169.png' 
+                          onClick={() => openFileInModal(file.webViewLink.replace('/view?usp=drivesdk', '/preview'))}  
+                      />
+                    }
+                    
+                    <div className="flex gap-2">                          
+                      <Button color="light">
+                          <a href={file.webViewLink} target="_blank" rel="noreferrer">View in Drive</a>
+                      </Button>
+                      <Button  onClick={()=>  openFileInModal(file.webViewLink.replace('/view?usp=drivesdk', '/preview'))} >
+                          Full View
+                        </Button>                    
+                      {
+                        currentUser&&
+                        <Button  onClick={()=> deleteFiles(file.id)} disabled={deleting}>
+                          Delete file
+                        </Button> 
+                      }               
+                              
+                    </div> 
+
+                  </Card>                  
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
 
         </div>
     )
