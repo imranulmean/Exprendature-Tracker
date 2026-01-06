92196ed6aad0181bedc5ed5c2da6a74277d5cb8b
Imranul Hasan <imranulhasan73@gmail.com>
2026-01-06 11:25:20 +0600

    changed in index

--- api/index.js
+++ api/index.js
@@ -35,6 +35,7 @@ app.use(cors({
   origin: ['http://localhost:5173',"https://exp-tracker-face.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 app.use(cookieParser());
