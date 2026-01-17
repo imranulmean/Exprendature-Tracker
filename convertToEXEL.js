1a1e3ea98a48655031d7ffe4a103f07af2c687a5
imranulmean <imranulhasan73@gmail.com>
2026-01-17 11:08:26 +0600

    Excel conversion

--- convertToEXEL.js
+++ convertToEXEL.js
@@ -1,9 +1,24 @@
 import XLSX from "xlsx";
 import fs from "fs";
 
-const jsonObj= fs.readFileSync('routers.json','utf8' );
+// const inputFile=`routers.json`;
+const inputFile=`FinalResult.json`;
+const jsonObj= fs.readFileSync(inputFile,'utf8' );
 const parsedJson= JSON.parse(jsonObj);
 const rows = parsedJson.routers;
+///////////////
+// const rows = parsedJson.map(item => {
+//     const r = item?.result;
+//     return {
+//         branchId: r?.branchId || "" ,
+//         router: r?.router || "",
+//         host: r?.host || "" ,
+//         routerType: r?.routerType || "" ,
+//         results: r?.results ? JSON.stringify(r.results, null, 2) : "No Results"
+//     };
+// });
+////////////////
+
 const worksheet = XLSX.utils.json_to_sheet(rows);
 const workbook = XLSX.utils.book_new();
 XLSX.utils.book_append_sheet(workbook, worksheet, "All Routers");
