19235e7d6c5904feb5041b715f647301bf2fdd7d
imranulmean <imranulhasan73@gmail.com>
2026-02-20 05:15:45 +0600

    alert

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,7 +1,8 @@
 import { useSelector } from 'react-redux';
 import { Outlet, Navigate } from 'react-router-dom';
 
-export default function PrivateRoute() {
+export default function PrivateRoute() {  
   const { currentUser } = useSelector((state) => state.user);
+  alert(JSON.stringify(currentUser))
   return currentUser ? <Outlet /> : <Navigate to='/login' />;
 }
