b6bdbf6bdda59bf87b6dc7d139a809f8b6cce2c0
Imranul Hasan <imranulhasan73@gmail.com>
2026-02-24 15:21:11 +0600

    Google Stun server added

--- client/src/pages/OnlineCall.jsx
+++ client/src/pages/OnlineCall.jsx
@@ -73,7 +73,16 @@ if (typeof global === 'undefined') {
       const peer = new Peer({
         initiator: true,
         trickle: false,
-        stream: stream
+        stream: stream,
+        config: {
+          iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'stun:stun1.l.google.com:19302' },
+            { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:stun3.l.google.com:19302' },
+            { urls: 'stun:stun4.l.google.com:19302' },
+          ],
+        },        
       });
   
       peer.on("signal", (data) => {
